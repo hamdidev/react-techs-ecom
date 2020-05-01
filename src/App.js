b473842b1a9834a4eceea5a8362d2dcc761a7ea2
hamdidev <itechtranss@gmail.com>
2020-05-01 11:26:48 +0300

    react icons added-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import { FaStoreAlt } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <h2>
-      Hi-TECHS Store <FaHome />
+      Hi-TECHS Store <FaStoreAlt />
     </h2>
   );
 }
